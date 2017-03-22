c7c002fe74a261d002da8854787073e38fd6748d
Bastian Kröger <bastian.kroeger@outlook.com>
2017-03-22 18:54:26 +0100

    added data readout and stock value calculation

---
+++ StockStrategies/Code/Stock.vb
@@ -0,0 +1,49 @@
+﻿Imports Newtonsoft.Json
+
+Public Class Stock
+
+    Public Property Name As String
+
+    <JsonProperty("GuV/Bilanz Operatives Ergebnis (EBIT)")>
+    Public Property Ebits As Dictionary(Of String, Double)
+
+    <JsonProperty("Aktie Dividende je Aktie")>
+    Public Property Dividends As Dictionary(Of String, Double)
+
+    <JsonProperty("Aktie Buchwert je Aktie")>
+    Public Property Bookvalues As Dictionary(Of String, Double)
+
+    <JsonProperty("Aktie Cashflow je Aktie")>
+    Public Property Cashflows As Dictionary(Of String, Double)
+
+    <JsonProperty("Bewertung Dividendenrendite in %")>
+    Public Property DividendYields As Dictionary(Of String, Double)
+
+    <JsonProperty("Bewertung KGV (Kurs/Gewinn)")>
+    Public Property Kgvs As Dictionary(Of String, Double)
+
+    <JsonProperty("Bewertung KUV (Kurs/Umsatz)")>
+    Public Property Kuvs As Dictionary(Of String, Double)
+
+    <JsonProperty("Bewertung KBV (Kurs/Buchwert)")>
+    Public Property Kbvs As Dictionary(Of String, Double)
+
+    <JsonProperty("Bewertung KCV (Kurs/Cashflow)")>
+    Public Property Kcvs As Dictionary(Of String, Double)
+
+    Public ReadOnly Property StockValues As Dictionary(Of String, Double)
+        Get
+            Dim r As New Dictionary(Of String, Double)
+            Dim v As Double
+
+            For Each kv In Me.Dividends
+                If Me.DividendYields.TryGetValue(kv.Key, v) Then
+                    r(kv.Key) = kv.Value * 100.0 / v
+                End If
+            Next
+
+            Return r
+        End Get
+    End Property
+
+End Class
--- StockStrategies/Program.vb
+++ StockStrategies/Program.vb
@@ -1,7 +1,23 @@
-﻿Module Program
+﻿Imports System.Text.RegularExpressions
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Module Program
 
     Sub Main()
+        Dim json As String = IO.File.ReadAllText(IO.Path.Combine(IO.Directory.GetCurrentDirectory, "Data", "dax formated.json"))
+        Dim normalized As String = Normalize(json)
+        Dim raw As Dictionary(Of String, Dictionary(Of String, Dictionary(Of String, String))) = JsonConvert.DeserializeObject(Of Dictionary(Of String, Dictionary(Of String, Dictionary(Of String, String))))(normalized)
+        Dim semiRaw As Dictionary(Of String, Stock) = JsonConvert.DeserializeObject(Of Dictionary(Of String, Stock))(normalized)
 
     End Sub
+
+    Public Function Normalize(json As String) As String
+        Return Regex.Replace(json, "(\d+),(\d+)", "$1.$2")
+    End Function
+
+    Public Function LoadDax() As Object
+
+    End Function
 
 End Module
